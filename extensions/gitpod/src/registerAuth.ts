e824a18263dfff39ed8ba7fc4b8a2fe40bdbae41
Filip Troníček <filip.tronicek@seznam.cz>
2021-09-23 16:31:00 +0000

    Resolve user info via GP protocol

--- extensions/gitpod/src/registerAuth.ts
+++ extensions/gitpod/src/registerAuth.ts
@@ -8,13 +8,24 @@ import ClientOAuth2 from 'client-oauth2';
 import * as vscode from 'vscode';
 import { URLSearchParams } from 'url';
 
-const authCompletePath = '/auth-complete';
+import { GitpodClient, GitpodServer, GitpodServiceImpl } from '@gitpod/gitpod-protocol/lib/gitpod-service';
+import { JsonRpcProxyFactory } from '@gitpod/gitpod-protocol/lib/messaging/proxy-factory';
+import WebSocket = require('ws');
+import ReconnectingWebSocket from 'reconnecting-websocket';
+import { ConsoleLogger, listen as doListen } from 'vscode-ws-jsonrpc';
 
-/*
+const authCompletePath = '/auth-complete';
+const baseURL = 'https://server-vscode-ouath2.staging.gitpod-dev.com';
+
 const scopes: string[] = [
 	'function:accessCodeSyncStorage'
 ];
-*/
+
+type UsedGitpodFunction = ['getWorkspace', 'openPort', 'stopWorkspace', 'setWorkspaceTimeout', 'getWorkspaceTimeout', 'getLoggedInUser', 'takeSnapshot', 'controlAdmission', 'sendHeartBeat', 'trackEvent'];
+type Union<Tuple extends any[], Union = never> = Tuple[number] | Union;
+export type GitpodConnection = Omit<GitpodServiceImpl<GitpodClient, GitpodServer>, 'server'> & {
+	server: Pick<GitpodServer, Union<UsedGitpodFunction>>
+};
 
 class UriEventHandler extends vscode.EventEmitter<vscode.Uri> implements vscode.UriHandler {
 	constructor(private readonly Logger: any) {
@@ -83,6 +94,56 @@ export function promiseFromEvent<T, U>(
 	};
 }
 
+export async function resolveAuthenticationSession(scopes: readonly string[], accessToken: string): Promise<vscode.AuthenticationSession> {
+	const factory = new JsonRpcProxyFactory<GitpodServer>();
+	const gitpodService: GitpodConnection = new GitpodServiceImpl<GitpodClient, GitpodServer>(factory.createProxy()) as any;
+
+	const pendignWebSocket = (async () => {
+		class GitpodServerWebSocket extends WebSocket {
+			constructor(address: string, protocols?: string | string[]) {
+				super(address, protocols, {
+					headers: {
+						'Origin': 'https://gitpod.io',
+						'Authorization': `Bearer ${accessToken}`
+					}
+				});
+			}
+		}
+		const webSocket = new ReconnectingWebSocket(baseURL.replace('https', 'wss'), undefined, {
+			maxReconnectionDelay: 10000,
+			minReconnectionDelay: 1000,
+			reconnectionDelayGrowFactor: 1.3,
+			connectionTimeout: 10000,
+			maxRetries: Infinity,
+			debug: false,
+			startClosed: false,
+			WebSocket: GitpodServerWebSocket
+		});
+		webSocket.onerror = console.error;
+		doListen({
+			webSocket,
+			onConnection: connection => factory.listen(connection),
+			logger: new ConsoleLogger()
+		});
+		return webSocket;
+	})();
+	const user = await gitpodService.server.getLoggedInUser();
+	(await pendignWebSocket).close();
+	return {
+		id: 'gitpod.user',
+		account: {
+			label: user.name!,
+			id: user.id
+		},
+		scopes: scopes,
+		accessToken: accessToken
+	};
+}
+
+function hasScopes(session: vscode.AuthenticationSession, scopes?: readonly string[]): boolean {
+	return !scopes || scopes.every(scope => session.scopes.includes(scope));
+}
+
 function registerAuth(context: vscode.ExtensionContext, logger: any): void {
 
 	const uriHandler = new UriEventHandler(logger);
@@ -103,26 +164,8 @@ function registerAuth(context: vscode.ExtensionContext, logger: any): void {
 		}
 	};
 
-	async function resolveAuthenticationSession(scopes: readonly string[], accessToken: string): Promise<vscode.AuthenticationSession> {
-		// Todo: use a real authentication session with @gitpod/protocol
-		return {
-			id: 'gitpod.user',
-			account: {
-				label: 'GitPod User',
-				id: 'gitpod.user'
-			},
-			scopes: scopes,
-			accessToken: accessToken
-		};
-	}
-
-	function hasScopes(session: vscode.AuthenticationSession, scopes?: readonly string[]): boolean {
-		return !scopes || scopes.every(scope => session.scopes.includes(scope));
-	}
-
-	async function createSession(scopes: string[]): Promise<vscode.AuthenticationSession> {
+	async function createSession(_scopes: string[]): Promise<vscode.AuthenticationSession> {
 		logger('Creating session...');
-		const baseURL = 'https://server-vscode-ouath2.staging.gitpod-dev.com';
 
 		const callbackUri = `${vscode.env.uriScheme}://gitpod.gitpod-desktop${authCompletePath}`;
 
@@ -134,7 +177,7 @@ function registerAuth(context: vscode.ExtensionContext, logger: any): void {
 			scopes: scopes,
 		});
 
-		const timeoutPromise = new Promise((_: (value: vscode.AuthenticationSession) => void, reject) => {
+		const timeoutPromise = new Promise((_: (value: vscode.AuthenticationSession) => void, reject): void => {
 			const wait = setTimeout(() => {
 				clearTimeout(wait);
 				reject('Login timed out.');
@@ -166,7 +209,9 @@ function registerAuth(context: vscode.ExtensionContext, logger: any): void {
 			return Promise.resolve(sessions.filter(session => hasScopes(session, scopes)));
 		},
 		createSession: async (scopes: string[]) => {
+			logger('Pushing change emitter');
 			context.subscriptions.push(onDidChangeSessionsEmitter);
+			logger('Returning create ');
 			return createSession(scopes);
 		},
 		removeSession: async () => {
